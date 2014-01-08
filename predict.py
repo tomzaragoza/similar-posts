76d670c9b209066a203398e97b35facc3ce4087a
Tom Zaragoza <tomdzaragoza@gmail.com>
2014-01-08 00:08:39 -0500

    add in calculations for each friend

--- predict.py
+++ predict.py
@@ -2,41 +2,33 @@ import os
 import sys
 import scipy as sp
 from pymongo import MongoClient
+from datetime import datetime
+from pprint import pprint as pretty
 from sklearn.feature_extraction.text import CountVectorizer
 from stemming import StemmedCountVectorizer
 vectorizer = StemmedCountVectorizer(min_df=1, stop_words="english") # minimum document frequency
 
-# print vectorizer
+# mongo setup
+mongo = MongoClient()
+db = mongo['predict-news-posts']
+collection = db['friends-posts-links']
+calculations = db['calculations']
 
-content = ["how to format my hard disk", "Hard disk format problems"]
+content = ["The Brain, in Exquisite Detail", "Asian Factories See Sense and Savings in Environmental Certification"]
 X = vectorizer.fit_transform(content)
-# print vectorizer.get_feature_names()
-# print X.toarray().transpose()
+print vectorizer.get_feature_names()
+print X.toarray().transpose()
 
 
 # DIR = "/home/tom/learning-ml-python/ch03/data/toy"
 # posts = [open(os.path.join(DIR, f)).read() for f in os.listdir(DIR)]
-posts = []
-for 
 
-X_train = vectorizer.fit_transform(posts)
-num_samples, num_features = X_train.shape
-
-print("#samples: %d, #features: %d" % (num_samples, num_features)) #samples: 5, #features: 25
-print vectorizer.get_feature_names()
-print "With {0} different words".format(len(vectorizer.get_feature_names()))
-
-# goal here is to find the most similar post for the post new_post defined below
-new_post = "imaging database"
-new_post_vec = vectorizer.transform([new_post])
-new_post_array = new_post_vec.toarray() # need it to be the full array if we want to do similarity calculations
-
-# calculate Euclidean distance, of new post with the old posts
+# # calculate Euclidean distance, of new post with the old posts
 def dist_raw(v1, v2):
 	delta = v1 - v2
 	return sp.linalg.norm(delta.toarray())
 
-# calculate the vector distance
+# # calculate the vector distance
 def dist_norm(v1, v2):
 	v1_normalized = v1 / sp.linalg.norm(v1.toarray())
 	v2_normalized = v2 / sp.linalg.norm(v2.toarray())
@@ -44,25 +36,47 @@ def dist_norm(v1, v2):
 	return sp.linalg.norm(delta.toarray())
 
 
-best_doc = None
-best_dist = sys.maxint
-best_i = None
+# get posts for each friend
+all_posts = {}
 
-print
+friend_posts = collection.find({})
 
-for i in range(0, num_samples):
-	post = posts[i] # iterate through the posts
-	if post == new_post:
-		continue
-	post_vec = X_train.getrow(i)
-	# d = dist_raw(post_vec, new_post_vec)
-	d = dist_norm(post_vec, new_post_vec)
-	print "=== Post {0} with dist={1}: {2}".format(i, d, post)
-	if d < best_dist:
-		best_doc = post
-		best_dist = d
-		best_i = i
-# this is before normalizing.
-print "The best post is {0}: '{1}' with dist={2}".format(best_i, best_doc, best_dist)
+for friend_post in list(friend_posts):
+	link_titles = []
+
+	for l in friend_post['links']:
+		if l['name'] and not l['name'] == 'Timeline Photos' and 'https://' not in l['name'] and 'http://' not in l['name']:
+			link_titles.append(l['name'])
+	all_posts[friend_post['name']] = link_titles
 
 
+# now to find out the distances for each friend's posts
+for name, posts in all_posts.iteritems():
+	# print name, posts
+	if len(posts) > 0:
+		for article_name in content:
+			print '{0}: for {1} -> article "{2}"'.format(datetime.now(), name.encode('utf-8'), article_name.encode('utf-8'))
+			X_train = vectorizer.fit_transform(posts)
+			num_samples, num_features = X_train.shape
+			
+			article_name_vec = vectorizer.transform([article_name])
+			article_post_array = article_name_vec.toarray()
+
+			best_doc = None
+			best_dist = sys.maxint
+			best_i = None
+
+			for i in range(0, num_samples):
+				post = posts[i]
+				if post == article_name:
+					continue
+				post_vec = X_train.getrow(i)
+				d = dist_norm(post_vec, article_name_vec)
+				# d = dist_raw(post_vec, article_name_vec)
+				print "=== Post {0} with dist={1}: {2}".format(i, d, post.encode('utf-8'))
+				if d < best_dist:
+					best_doc = post
+					best_dist = d
+					best_i = i
+			if best_dist > 0.0:
+				calculations.insert({'name': name, 'content-test': article_name, 'best-post': best_doc, 'best-distance': best_dist})
